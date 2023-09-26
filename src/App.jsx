35a4e101aab544f8904390335603d109919f3671
Neel Bamroliya <neelbamroliya0@gmail.com>
2023-09-26 23:01:36 +0530

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar title="My App" />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* passing props from here to navbar component */}
+            <Navbar title="My App" />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-
-const Navbar = ({ title }) => {
-  return (
+const Navbar = ({ title }) => {  //getting props here
+    return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
+                {/* setting props here */}
                 <a className="navbar-brand" href="/">{title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -26,13 +26,15 @@ const Navbar = ({ title }) => {
                 </div>
             </div>
         </nav>
-  )
+    )
 }
 
+// proptypes validations
 Navbar.prototype = {
     title: PropTypes.string.isRequired
 }
 
+// default props setting
 Navbar.defaultProps = {
     title: 'Your App Name',
 }
